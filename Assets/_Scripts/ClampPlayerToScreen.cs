054128838272b0c93f8c74e4189eb2137dbe1e24
MasterKrepta <brianj.pajak@gmail.com>
2020-02-01 02:18:55 -0500

    start of debris mechanics

---
+++ Assets/_Scripts/ClampPlayerToScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClampPlayerToScreen : MonoBehaviour
+{
+    Vector2 screenBounds;
+    float playerWidth;
+    float playerHeight;
+
+    private void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        playerWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        playerHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+    }
+    // Update iUpdates called once per frame
+    void LateUpdate()
+    {
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x + playerWidth, screenBounds.x * -1 - playerWidth);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y + playerHeight, screenBounds.y * -1 - playerWidth);
+        transform.position = viewPos;
+    }
+}
---
+++ Assets/_Scripts/DebrisSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebrisSpawner : MonoBehaviour
+{
+    [SerializeField] Transform debris;
+    [SerializeField] float spawnRate = 2.5f;
+    float nextSpawntime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", 1.0f, spawnRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+    private void Spawn()
+    {
+
+        Vector3 screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width),  Screen.height/* Random.Range(0, Screen.height)*/, /*Camera.main.farClipPlane / 2*/0));
+        screenPosition.z = 0;
+        screenPosition.y = 155; //todo this is hacky
+       Instantiate(debris, screenPosition, Quaternion.identity);
+        
+    }
+}
+
