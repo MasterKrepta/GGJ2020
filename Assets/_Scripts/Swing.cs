bc9b52f252f1b304dff676f1fdcc59c244f51d31
MasterKrepta <brianj.pajak@gmail.com>
2020-01-31 22:28:30 -0500

    start of swing

---
+++ Assets/_Scripts/Swing.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swing : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public LineRenderer line;
+    DistanceJoint2D joint;
+    Vector3 targetPos;
+    RaycastHit2D hit;
+    float distance = 100f;
+    public LayerMask mask;
+    public float step = 0.2f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        joint = GetComponent<DistanceJoint2D>();
+        joint.enabled = false;
+        line.enabled = false;
+    }
+    private void Update()
+    {
+
+        if (joint.distance > 1f)
+        {
+            joint.distance -= step;
+        }
+        else
+        {
+            line.enabled = false;
+            joint.enabled = false;
+           
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPos.z = 0;
+            
+            hit = Physics2D.Raycast(transform.position, targetPos - transform.position, distance, mask);
+
+            if (hit.collider != null)
+            {
+                print($"we hit {hit.transform.name}");
+
+                joint.enabled = true;
+                joint.connectedBody = hit.collider.gameObject.GetComponent<Rigidbody2D>();
+                joint.connectedAnchor = hit.point - new Vector2(hit.collider.transform.position.x, hit.collider.transform.position.y);
+                joint.distance = Vector2.Distance(transform.position, hit.point);
+
+                line.enabled = true;
+                line.SetPosition(0, transform.position);
+                line.SetPosition(1, hit.point);
+            }
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            line.SetPosition(0, transform.position);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            joint.enabled = false;
+            line.enabled = false;
+        }
+    }
+
+
+}
+
